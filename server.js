26e8a1fb05d4edb02825c9a91ff40c04552e2248
misha <belokonm@gmail.com>
2020-04-03 12:40:22 +0200

    added Jexia JS SDK into example

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
-'use strict';
+const express = require("express");
+const dotenv = require("dotenv"); 
+dotenv.config();
 
-const express = require('express');
+const jexiaSDK = require("jexia-sdk-js/node");
+const dataModule = jexiaSDK.dataOperations();
+const field = jexiaSDK.field();
 
-// Constants
-const PORT = 80;
+const credentials = {
+  projectID: process.env.PROJECT_ID,
+  key: process.env.API_KEY,
+  secret:process.env.API_SECRET
+};
+
+jexiaSDK.jexiaClient().init( credentials,dataModule);
+const todo = dataModule.dataset(process.env.DATASET);
+
+// to deploy it to AppHosting please use port 80
+const PORT = 3000; 
+
+// to deploy it to AppHosting please use IP:0.0.0.0
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  todo
+  .select()
+  .limit(200)
+  .fields("text", "id","done")
+  .where(field=> field("text").isLike("%4").and(field("id").isNotNull()))
+  .subscribe(
+   records => { 
+    console.log(records)
+    res.send(records)
+  }, 
+  error => { 
+     console.log(error)
+     next(error)
+  }
+ );  
 });
 
 app.listen(PORT, HOST);
